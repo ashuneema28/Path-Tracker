ccf4f58942a796defcd2219e16c7b8e22bf6c69b
ashuneema28 <ashu.neema28@gmail.com>
2022-06-07 04:30:40 -0600

    Adding Bcrypt password saving and authentication

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -19,4 +19,26 @@ router.post('/signup', async (req, res) => {
     }
 });
 
+router.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+    
+    if(!email || !password){
+        return res.status(422).send({error: "Must provide email and password"});
+    }
+
+    const user = await User.findOne({email : email});
+
+    if(!user){
+        return res.status(422).send({error: "Invalid Password or email"});
+    }
+
+    try{
+        await user.comparePassword(password);
+        const token = jwt.sign({userId : user._id}, 'MY_SECRET_KEY');
+        res.send({token : token});
+    }catch (err){
+        return res.status(422).send({error : "Invalid Password or email"});
+    }
+});
+
 module.exports = router;
