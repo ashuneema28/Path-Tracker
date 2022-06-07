655674d1575de7456a565d5c53c700e01cf20663
ashuneema28 <ashu.neema28@gmail.com>
2022-06-07 02:54:44 -0600

    adding connection to mongoDb instance

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri = 'mongodb+srv://admin:12345@path-tracker-cluster.jtiwi.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected' , () => {
+    console.log('Connected to Mongo Instance')
+});
+
+mongoose.connection.on('error' , (err) => {
+    console.log('Error connecting to Mongo Instance' , err);
+});
 
 app.get('/' , (req, res) => {
     res.send("Hello there!!!!");
