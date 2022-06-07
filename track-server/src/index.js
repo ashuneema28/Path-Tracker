e0bd82ba1bb87b3d1ddbb37700faf6a26b7094e7
ashuneema28 <ashu.neema28@gmail.com>
2022-06-07 03:02:57 -0600

    Creating a authentication route

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:12345@path-tracker-cluster.jtiwi.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoUri);
